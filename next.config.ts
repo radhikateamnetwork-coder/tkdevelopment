7699f7e24ea3fa41b7a31eb72e8502fd4ad175af
tusharkumar888 <tusharkumar888219@gmail.com>
2026-02-18 20:23:02 +0530

    out dir

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    images: {
-    domains: ["images.unsplash.com"],
-  },
+  output: "export",
+images: {
+  unoptimized: true,
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "images.unsplash.com",
+      pathname: "/**",
+    },
+  ],
+}
+
 };
 
 export default nextConfig;
